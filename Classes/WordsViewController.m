76f348753f1d29408a549015d15695ff832e77ea
David Stalnaker <david.stalnaker@gmail.com>
2011-01-08 17:26:48 -0500

    made the web query actually populate the words in the ui

--- Classes/WordsViewController.m
+++ Classes/WordsViewController.m
@@ -96,6 +96,12 @@
 	NSString *jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
 	NSMutableArray* decoded = [parser objectWithString: jsonString];
 	NSLog(@"%@", decoded);
+	for (NSMutableDictionary* elem in decoded) {
+		[newWords addObject:[[Word alloc] initWithWord:[[elem valueForKey:@"word"] copy]
+											definition:[[elem valueForKey:@"definition"] copy]]];
+	}
+	[self setNewWord];
+	[word becomeFirstResponder];
 }
 
 
@@ -106,34 +112,12 @@
 	
 	points = 0;
 	
-	[self getNewWords];
-	
 	newWords = [[NSMutableArray alloc] initWithCapacity:20];
 	pastWords = [[NSMutableArray alloc] initWithCapacity:20];
 	
-	[newWords addObject:[[Word alloc] initWithWord:@"amnesty" definition:@"An official pardon for people who have been convicted of political offenses"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"hunger" definition:@"A feeling of discomfort or weakness caused by lack of food, coupled with the desire to eat"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"pajamas" definition:@"A suit of loose pants and jacket or shirt for sleeping in"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"seriously" definition:@"In a solemn or considered manner"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"flower" definition:@"A brightly colored and conspicuous example of such a part of a plant together with its stalk, typically used with others as a decoration or gift"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"enlist" definition:@"Enroll or be enrolled in the armed services"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"logical" definition:@"Characterized by clear, sound reasoning"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"attachment" definition:@"An extra part or extension that is or can be attached to something to perform a particular function"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"classic" definition:@"Judged over a period of time to be of the highest quality and outstanding of its kind"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"rag" definition:@"A piece of old cloth, esp. one torn from a larger piece, used typically for cleaning things"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"film" definition:@"A thin flexible strip of plastic or other material coated with light-sensitive emulsion for exposure in a camera, used to produce photographs or motion pictures"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"shelf" definition:@"A flat length of wood or rigid material, attached to a wall or forming part of a piece of furniture, that provides a surface for the storage or display of objects"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"resume" definition:@"Begin to do or pursue (something) again after a pause or interruption"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"flexibility" definition:@"The quality of bending easily without breaking"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"consent" definition:@"Give permission for something to happen"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"editor" definition:@"A person who is in charge of and determines the final content of a text, particularly a newspaper or magazine"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"shower" definition:@"An enclosure in which a person stands under a spray of water to wash"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"choice" definition:@"A course of action, thing, or person that is selected or decided upon"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"building" definition:@"A structure with a roof and walls, such as a house, school, store, or factory"]];
-	[newWords addObject:[[Word alloc] initWithWord:@"parallel" definition:@"(of lines, planes, surfaces, or objects) Side by side and having the same distance continuously between them"]];
+	[self getNewWords];
 	
-	[self setNewWord];
-	[word becomeFirstResponder];
+	
 	[self resetTimer];
 }
 
